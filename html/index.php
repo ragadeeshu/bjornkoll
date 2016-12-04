4673bb920e9de8de7fceae34fd17f1e9ef8f78da
Ragnar Mellbin <ragnar.mellbin@neotechnology.com>
2016-12-04 10:21:38 +0100

    Add 100 week change

--- html/index.php
+++ html/index.php
@@ -1,3 +1,30 @@
+<?php
+$response;
+$body_text = "";
+exec("python3 ../bjornkoll.py", $response);
+$size = 40;
+$scoreline = array_shift($response);
+$split = explode ( "<br>", $scoreline);
+$last_word;
+if( sizeof($split) >1){
+  $body_text = $body_text . "<p style=\"font-size:${size}px\">$split[0]</p>";
+  $body_text = $body_text . "<p id='score' style=\"font-size:${size}px\">$split[1]</p>";
+  $last_word = explode ( " ", $split[1])[2];
+} else {
+  $body_text = $body_text . "<p id='score' style=\"font-size:${size}px\">$split[0]</p>";
+  $last_word = explode ( " ", $split[0])[2];
+}
+$size += 30;
+for ($i = 0; $i < count($response); ++$i) {
+  $body_text = $body_text . "<pre style=\"font-size:${size}px;\">";
+  $body_text = $body_text . "<span>$response[$i]</span>";
+  $i++;
+  $body_text = $body_text . "<span class=\"shh\">$response[$i]</span>";
+  $body_text = $body_text . "</pre>\n";
+  $size *= 0.95;
+}
+
+?>
 <!DOCTYPE html>
 <html>
 <style> a:link {color:#FFE14B; background-color:transparent;
@@ -21,23 +48,48 @@ font-family: 'Courier New', Courier, monospace;
 
 pre:hover .shh { display: inline }
 
+<?php if(intval($last_word)>=100){?>
+#score {
+
+    -webkit-animation: colorRotate 6s linear 0s infinite;
+     -moz-animation: colorRotate 6s linear 0s infinite;
+       -o-animation: colorRotate 6s linear 0s infinite;
+          animation: colorRotate 6s linear 0s infinite;
+
+}
+
+@keyframes colorRotate {
+    from {
+        color: rgb(255, 0, 0);
+    }
+    16.6% {
+        color: rgb(255, 0, 255);
+    }
+    33.3% {
+        color: rgb(0, 0, 255);
+    }
+    50% {
+        color: rgb(0, 255, 255);
+    }
+    66.6% {
+        color: rgb(0, 255, 0);
+    }
+    83.3% {
+        color: rgb(255, 255, 0);
+    }
+    to {
+        color: rgb(255, 0, 0);
+    }
+<?php
+}
+?>
+
 </style>
 <head>
-  <title>Deeshu's bear watcher</title> </head> <body>
+  <title>Deeshu's bear watcher</title>
+</head>
+<body>
   <h1>Update statistics for <a href="http://www.thebearden.se/">thebearden.se</a></h1>
-  <?php
-  $response;
-  exec("python3 ../bjornkoll.py", $response);
-  $size = 40;
-  $scoreline = array_shift($response);
-  print "<p style=\"font-size:${size}px\">$scoreline</p>";
-  $size += 30;
-  for ($i = 0; $i < count($response); ++$i) {
-    print "<pre style=\"font-size:${size}px;\">";
-    print "<span>$response[$i]</span>";
-    $i++;
-    print "<span class=\"shh\">$response[$i]</span>";
-    print "</pre>";
-    $size *= 0.95;
-  }
-  ?> </body> </html>
+  <?=$body_text?>
+ </body>
+ </html>
